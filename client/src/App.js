77570a6ba84c473176dd77d7b19a2a9046dfce29
Vince216 <vinceshury@gmail.com>
2019-06-12 15:55:01 -0400

    initial files setup with boilerplate code in place, along with mongo Model setup.

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <h1>HELLOWORLD</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
