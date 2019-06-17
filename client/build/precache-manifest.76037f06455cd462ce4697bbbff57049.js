2ac569f19338ca0feea58c7c3f9cd61cf34f23eb
Vince216 <vinceshury@gmail.com>
2019-06-17 12:06:18 -0400

    removed file contents starting with file structure and getting a static page

---
+++ client/build/precache-manifest.76037f06455cd462ce4697bbbff57049.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9408aca958bfb2ccb43b",
+    "url": "/static/js/main.44ef58bd.chunk.js"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "6120d0139302ae62ee93fc767260bfee",
+    "url": "/index.html"
+  }
+];
--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,11 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React from "react";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <h1>HELLOWORLD</h1>
-      </div>
-    );
-  }
+
+function App() {
+  return (
+<h1>My App!</h1>
+  );
 }
 
 export default App;
