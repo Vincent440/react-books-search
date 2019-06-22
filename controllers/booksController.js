41a5aa11349c723d591f37b7ef6c195becf08f48
Vince216 <vinceshury@gmail.com>
2019-06-21 21:49:48 -0400

    cleaning up files

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -2,11 +2,11 @@ const db = require("../models/book.js");
 
 module.exports = {
     findAll: function (req, res) {
-        db.find(req.query).sort({ date: -1 }).then(data =>res.status(200).json(data))
+        db.find(req.query).sort({ date: -1 }).then(data => res.status(200).json(data))
         .catch(err => res.status(400).json(err));
     },
     create: function (req, res) {
-        db.create(req.body).then(data =>res.status(200).json(data))
+        db.create(req.body).then(data => res.status(200).json(data))
         .catch(err => res.status(400).json(err));
     },
     remove: function (req, res) {
--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
-const path = require("path");
 const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/googlebooks";
 
@@ -11,12 +10,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Connect to the Mongo DB
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useFindAndModify: false  }, err => {
-  if (err) {
-      console.log(err);
-  } else {
-      console.log("Mongoose connected to the DB successfully!");
-  }
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useFindAndModify: false },err => {
+  console.log( err ? err : "Mongoose connected to the DB successfully!");
 });
 
 if (process.env.NODE_ENV === "production") {
@@ -26,10 +21,5 @@ if (process.env.NODE_ENV === "production") {
 // Add routes, both API and view
 app.use(routes);
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
 // Start the API server
-app.listen(PORT, ()=>{
-  console.log(`App API ==>  server now on http://localhost:${PORT}!`);
-});
+app.listen(PORT, () => console.log(`App API ==>  server now on http://localhost:${PORT}!`));
