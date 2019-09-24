0c8edc079636872991b2c716e915f1fa7c3444d3
vince s <vinceshury@gmail.com>
2019-09-24 11:42:43 -0400

    changed server js db config, updated server NPM

--- server.js
+++ server.js
@@ -4,23 +4,17 @@ const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/googlebooks";
-
+const mongooseConfig = { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false };
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Connect to the Mongo DB
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useFindAndModify: false }, error =>{ 
-  console.log( error ? error : "Mongoose connected to the DB successfully!");
-});
+mongoose.connect(MONGODB_URI, mongooseConfig, error => console.log(error ? error : "--Connected to Database"));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Add routes, both API and view
 app.use(routes);
 
-// Start the API server
-app.listen(PORT, () => {
-  console.log(`\nAPI server ON ==> http://localhost:${PORT}!\n`)
-});
+app.listen(PORT, () => console.log(`--API RUNNING ON ==> Port = ${PORT}`));
